8b90b92ea31c638caf0ecba4c145662bd60312e4
Joe P <joseph.r.paradiso@gmail.com>
2022-01-19 22:18:13 -0500

    adjust build obstacles

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-
-// let prisma: PrismaClient;
-
 declare global {
-  var prisma: PrismaClient;
+	var prisma: PrismaClient | undefined;
 }
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
