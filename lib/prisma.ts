f783c672096c7ba8662b78e60cbd51ee945af853
Joe P <joseph.r.paradiso@gmail.com>
2022-01-20 00:58:17 -0500

    force a rebuild

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,8 @@ declare global {
 }
 
 const prisma = global.prisma || new PrismaClient({ log: ["info"] });
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
 
 export default prisma;
